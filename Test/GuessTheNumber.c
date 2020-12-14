876155a400c8c39c91565e5ba53f6617ea760d9a
低调的小黑孩 <42669193+zhemeduogewangtianyi@users.noreply.github.com>
2020-12-14 17:12:20 +0800

    Create GuessTheNumber.c

---
+++ Test/GuessTheNumber.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int gameEnter(int range) {
+	
+	int random = initRandomSpeed();
+
+	menu(range);
+
+	return 0;
+}
+
+int initRandomSpeed() {
+
+	typedef unsigned int u_int;
+
+	int random = (u_int)time(0);
+
+	srand(random);
+
+	return random;
+
+}
+
+int GeneratorRandom(int range) {
+
+	return rand() % range + 1;
+	
+}
+
+int menu(int range) {
+	
+agin:
+	printf("********************************\n");
+	printf("*******	1:start 2:end	********\n");
+	printf("********************************\n");
+	printf("please select >:");
+
+	int in = 0;
+
+in:	
+	scanf("%d", &in);
+
+	switch (in) {
+	case 1:
+		start(range);
+		goto agin;
+	case 2:
+		break;
+	default:
+		printf("not found selected !  please try agin onece ! \n");
+		goto in;
+	}
+	
+}
+
+
+int start(int range) {
+
+	int number = GeneratorRandom(range);
+
+	printf("please enter guess number ! \n");
+
+	while (1){
+
+		int guessNumber = 0;
+
+		scanf("%d", &guessNumber);
+
+		if (number > guessNumber) {
+			printf("It's small ! \n");
+		}
+		else if (number < guessNumber) {
+			printf("It's big ! \n");
+		}
+		else {
+			printf("\r\n yes ! It's %d \r\n\r\n", guessNumber);
+			break;
+		}
+	}
+	
+}
